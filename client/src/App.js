5c3d8ddf205a2eff863eacc9499642463ec5e524
adamblgh <94824058+adamblgh@users.noreply.github.com>
2022-12-15 20:25:59 +0100

    icon_homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
-import { Home } from "./components/Home"
+import "./App.css";
+import { Home } from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+        </Routes>
+    </QueryClientProvider>
   );
 }
 
