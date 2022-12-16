c615f1fdb338360afbc76dd55f399c657fcef00e
adamblgh <94824058+adamblgh@users.noreply.github.com>
2022-12-17 00:12:31 +0100

    homepage_and_logo

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import { Home } from "./components/Home";
+import { Register } from "./components/Register.jsx";
 import { Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
-import { Register } from "./components/Register";
 
 const queryClient = new QueryClient();
 
@@ -13,7 +13,7 @@ function App() {
   const [loggedInUser, setLoggedInUser] = useState({});
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="holder d-flex justify-content-center">
+      <div className="d-flex justify-content-center box">
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/register" element={<Register />}/>
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { motion } from "framer-motion";
 import { useMutation } from "react-query";
 import { Register } from "./Register";
-import { validate } from 'react-email-validator';
+import { validate } from "react-email-validator";
 import {
   Form,
   FormGroup,
@@ -12,41 +12,63 @@ import {
   Button,
 } from "reactstrap";
 import { useNavigate } from "react-router-dom";
-  
+import bg from "../components/background/bg.mp4";
 
 export const Home = ({ setLoggedInUser }) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div>
-      <div className="col-12">
-      <h1 className="mt-2 title lineUp">
-        SPECIALIST LAB™
-      </h1>  
-      </div>
-      <div className="row lineUp">
-      
-        {/*REGISZTRÁCIÓ*/}
-        <div className="col-md-6">
-          <motion.div
-            whileHover={{ scale: [null, 1.1] }}
-            transition={{ duration: 1 }}
-            onClick={()=>navigate('/register')}
-            className="panel register p-3 shadow mt-1 rounded"
-          >
-            <h3 className="kitoltes text-center">Regisztráció</h3>
-          </motion.div>
+      <video
+        autoPlay
+        loop
+        muted
+        style={{
+          position: "absolute",
+          width: "100%",
+          left: "50%",
+          top: "50%",
+          height: "100%",
+          objectFit: "cover",
+          transform: "translate(-50%, -50%)",
+          zIndex: "-1",
+        }}
+      >
+        <source src={bg} type="video/mp4" />
+      </video>
+      <div className="row box justify-content-center align-items-center">
+        {/*CÍM*/}
+        <div className="col-md-4">
+          <h1 className="mt-2 titleh1 lineUp">
+            SPECIALIST LAB™
+          </h1>
         </div>
+        {/*PANELEK*/}
+        <div className="col-md-8">
+          <div className="row lineUp">
+            {/*REGISZTRÁCIÓ*/}
+            <div className="col-md-6">
+              <motion.div
+                whileHover={{ scale: [null, 1.03, 1.06, 1.1] }}
+                transition={{ duration: 0.2 }}
+                onClick={() => navigate("/register")}
+                className="panel register p-3 mt-1 rounded"
+              >
+                <h3 className="kitoltes text-center">Regisztráció</h3>
+              </motion.div>
+            </div>
 
-        {/*BEJELENTKEZÉS*/}
-        <div className="col-md-6">
-          <motion.div
-            whileHover={{ scale: [null, 1.1] }}
-            transition={{ duration: 1 }}
-            className="panel login p-3 mt-1 rounded"
-          >
-            <h3 className="kitoltes text-center">Bejelentkezés</h3>
-          </motion.div>
+            {/*BEJELENTKEZÉS*/}
+            <div className="col-md-6">
+              <motion.div
+                whileHover={{ scale: [null, 1.03, 1.06, 1.1] }}
+                transition={{ duration: 0.2 }}
+                className="panel login p-3 mt-1 rounded"
+              >
+                <h3 className="kitoltes text-center">Bejelentkezés</h3>
+              </motion.div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
