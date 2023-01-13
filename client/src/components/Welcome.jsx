b2608c0e0b6e5281a3986d0db5bb5981d736d002
Czeczon Kristóf <kczeczon1@gmail.com>
2023-01-13 12:03:03 +0100

    bgvideo

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -26,13 +26,6 @@ export const Welcome = ({ setLoggedInUser }) => {
         loop
         muted
         style={{
-          position: "absolute",
-          width: "100%",
-          left: "50%",
-          top: "50%",
-          height: "100%",
-          objectFit: "cover",
-          transform: "translate(-50%, -50%)",
           zIndex: "-1",
         }}
       >
